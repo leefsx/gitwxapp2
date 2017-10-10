fc61aa6991edb3d9ed8caddd16b29bc439c0b08d
leefsx <leefsx@aliyun.com>
2017-10-10 19:05:14 +0800

    多点击几次可能会异常

--- pages/component/category/category.js
+++ pages/component/category/category.js
@@ -388,7 +388,8 @@ Page({
     var curIndex = e.currentTarget.dataset.index;
     that.setData({
       curIndex: curIndex,
-      category_name:category_name
+      category_name:category_name,
+      loading: false
     })
     app.request({
       url: app.domain + '/api/product/list',
